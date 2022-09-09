072aa2879bbbd3702795d4c895d09d39f830c3eb
eugene zotov <evgzai.87@yandex.ru>
2022-09-09 12:52:30 +0300

    New comment system. 1. Created new model Comment. 2. Created template for user's comments. 3. Re-wrote view function for the post detail. 4. Created comment form. 5. Added css classes to the comments page.

--- blogz/blog/forms.py
+++ blogz/blog/forms.py
@@ -1,7 +1,4 @@
-from django.contrib.auth.models import User
-from django import forms
-from django.forms import ModelForm, Form
-from django.forms import Form
+from django.forms import ModelForm
 from .models import Post, Comment
 
 
